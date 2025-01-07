d99b7612030b64bb2723ed8b63795b76097e97ff
kanishka <skanishka9560@gmail.com>
2025-01-07 17:18:52 +0530

    updated

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -108,13 +108,13 @@ const MyChats = ({fetchAgain}) => {
                   ? getSender(loggedUser, chat.users)
                   : chat.chatName }
               </Text>
-              {chat.latestMessage && (
+              {/* {chat.latestMessage && (
                 <Text  fontSize="xs">
-                  <b>{chat.latestMessage.sender?.name} : </b>
+                  <b>{chat.latestMessage.sender.name} : </b>
                   {chat.latestMessage.content.length > 50
                     ? (chat.latestMessage.content.substring(0,51) + "...") : (chat.latestMessage.content)}
                 </Text>
-              )}
+              )} */}
             </Box>
            ))}
         </Stack>
